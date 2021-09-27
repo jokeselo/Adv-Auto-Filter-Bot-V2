9b0ac27d1357691505c61c5d58e55b2cb495a64f
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-27 08:04:02 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -70,11 +70,11 @@ async def auto_filter(bot, update):
             
             if file_size < 1024:
                 file_size = f"🗂️[{file_size} B]"
-            elif file_size < (🗂️1024**2🗂️):
+            elif file_size < (1024**2):
                 file_size = f"🗂️[{str(round(file_size/1024, 2))} KB] "
-            elif file_size < (🗂️1024**3🗂️):
+            elif file_size < (1024**3):
                 file_size = f"🗂️[{str(round(file_size/(1024**2), 2))} MB] "
-            elif file_size < (🗂️1024**4🗂️):
+            elif file_size < (1024**4):
                 file_size = f"🗂️[{str(round(file_size/(1024**3), 2))} GB] "
             
             
