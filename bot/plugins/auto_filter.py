5b7dc476127753a8dadcc3050c1d334e84852b56
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-27 08:27:16 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"🎬{file_name}\n🎬\n🗂️{file_size}\n🗂️"
+            button_text = f"🗂️{file_size}🗂️ - 🎬{file_name}🎬"
             
 
             if file_type == "video":
